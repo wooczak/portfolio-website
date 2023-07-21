3734b42d4ed75176cfee9757796d709735cc8e0f
wooczak <kacper.luczak99@gmail.com>
2023-07-21 19:45:13 +0200

    Creating a mobile nav placeholder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
@@ -9,7 +10,13 @@ export default {
       fontFamily: {
         'cabinet-grotesk': ['Cabinet grotesk', 'sans-serif'],
         'jet-brains': ['JetBrains Mono', 'monospace']
-      }
+      },
+      height: {
+        'mobile-nav': 'calc(100vh - 3.5rem)'
+      },
+      margin: {
+        'vertical-gap': 'var(--vertical-gap)'
+      },
     },
   },
   plugins: [],
